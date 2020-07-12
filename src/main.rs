e718a446e4f9da4678affdd88ca2d088cef2ce57
Clemens Tiedt <clemens.tiedt@student.hpi.de>
2020-07-13 00:42:16 +0200

    add argument number check to binary

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use wrapgen::WrapGen;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() < 3 {
+        println!("Invoke wrapgen with `wrapgen <input.rs> <output.rs>`");
+        return;
+    }
+
     WrapGen::new(&args[1])
         .prefix("rs_")
         .use_core(true)
